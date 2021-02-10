502ee96ded268c1f60b35d88c92f4ae845b9f045
lpatipat <pluangwirun@drew.edu>
2021-02-09 22:59:02 -0500

    3. Redux setup and devtool complete

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+//Redux SetUp
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import {} from "react-redux";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
