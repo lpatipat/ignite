c26106de37f158ced720cc35dfbadeb4fd9652a4
lpatipat <pluangwirun@drew.edu>
2021-02-10 11:04:03 -0500

    6. fetching other games. bad: 3 arrays in 1state

--- src/api.js
+++ src/api.js
@@ -29,7 +29,16 @@ const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 //Next years days months
 const NextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
 //Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size10`;
+//Upcoming Games
+const upcoming_games = `games?dates=${currentYear},${NextYear}&ordering=-added&page_size=10`;
+//NEW GAMES
+const newGames = `games?date=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
+
+console.log(upcomingGamesURL);
