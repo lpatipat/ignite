45986bf5eecc5ac704bfcc29abc9e653cfacf177
lpatipat <pluangwirun@drew.edu>
2021-02-10 18:34:50 -0500

    9. global styles integrated

---
+++ src/components/Game.js
@@ -0,0 +1,28 @@
+import React from "react";
+//Styling and Animations
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Game = ({ name, released, key, id, image }) => {
+  return (
+    <StyledGame>
+      <h3>{name}</h3>
+      <p>{released}</p>
+      <img src={image} alt={name} />
+    </StyledGame>
+  );
+};
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  img {
+    width: 100%;
+    height: 50vh;
+    object-fit: cover;
+    padding-bottom: 2rem;
+  }
+`;
+
+export default Game;
