a2661704d18f8f72b49ef92e56d9b4ba35556008
lpatipat <pluangwirun@drew.edu>
2021-02-10 22:25:42 -0500

    15. Toggling components (modal)

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,6 +12,7 @@ const Game = ({ name, released, id, image }) => {
   //load details
   const dispatch = useDispatch();
   const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
   return (
@@ -29,6 +30,8 @@ const StyledGame = styled(motion.div)`
   box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 1rem;
+  cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     height: 50vh;
